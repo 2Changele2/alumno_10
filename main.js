8a2c570e7c138657b863d36c6af76cbdea3393a8
Juampa <juan.pa.pe.co.98@gmail.com>
2022-05-18 13:18:07 +0200

    Con el par o impar arreglado ya

--- main.js
+++ main.js
@@ -1,8 +1,54 @@
-window.onload = function()
-{
-    var el = document.getElementById("add-task");
-    el.onclick = function()
+window.onload = function () {
+    var contador = 0;
+    var btn = document.getElementById("add-task");
+    var areaTexto = document.getElementById("nueva-tarea");
+    var listaDesordenadaPar = document.getElementById("lista-rellenar");
+    var listaDesordenadaImpar = document.getElementById("lista-rellenar");
+
+    btn.onclick = function () {
+        contador++;
+
+        var contadorClasifica = contador % 2;
+        var nuevoTexto = areaTexto.value;
+
+        var nuevoItemListaPar = document.createElement("li");
+        var nuevoItemListaImpar = document.createElement("li");
+
+
+
+        var contenidoNuevoPar = document.createTextNode(nuevoTexto);
+        var contenidoNuevoImpar = document.createTextNode(nuevoTexto);
+
+
+
+        nuevoItemListaPar.className = "el-lista-par";
+        nuevoItemListaImpar.className = "el-lista-impar";
+
+
+        if (contadorClasifica == 0) 
         {
-        console.log("click violento");
+            nuevoItemListaPar.appendChild(contenidoNuevoPar);
+            listaDesordenadaPar.append(nuevoItemListaPar);
+
         }
+        else
+        {
+            nuevoItemListaImpar.appendChild(contenidoNuevoImpar);
+            listaDesordenadaImpar.append(nuevoItemListaImpar);
+
+        }
+         
+
+
+        {
+            nuevoItemLista.appendChild(contenidoNuevo);
+            listaDesordenada.append(nuevoItemLista);
+
+        }
+
+
+
+
+
+    }
 }
